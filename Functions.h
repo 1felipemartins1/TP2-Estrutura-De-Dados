de1fd3eea2852914718f4a733da61e9bf62c0511
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2024-07-11 16:51:44 -0300

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,14 +1,26 @@
-#ifndef GRAPH_H
-#define GRAPH_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
 struct Coordinate {
-    int x, y;
+    float x, y;
 };
 
 struct Node {
-    int vertex; 
-    double weight; // Peso da aresta
-    Node* next; 
+    int vertex;
+    float weight; // Peso da aresta
+    Node* next;
+};
+
+struct Portal {
+    int u; 
+    int v; 
+    float weight; 
+};
+
+struct Edge {
+    int u;
+    int v;
+    float weight;
 };
 
 class Graph {
@@ -16,7 +28,10 @@ private:
     Node** adj; // Ponteiro para array de ponteiros para nós
     Coordinate* coordinates; // Array de coordenadas
     int numVertices; // Número de vértices no grafo
-
+    Edge* edges; // Array de pares de arestas
+    Portal* portal; // Array de portais 
+    int edgeCount; // Contador de arestas
+    int portalCount; // Contador de portais 
 public:
     // Construtor da classe
     Graph(int vertices);
@@ -27,8 +42,14 @@ public:
     // Função para adicionar uma aresta direcionada do vértice u para o vértice v
     void addEdge(int u, int v);
 
+    // Função para adicionar uma aresta portal direcionada do vertice u para o vértice v (portais tem weight = 0)
+    void addPortal(int u, int v);
+
     // Função para definir a coordenada de um vértice
-    void setCoordinate(int vertex, int x, int y);
+    void setCoordinate(int vertex, float x, float y);
+
+    // Função para calcular a distância euclidiana entre dois vértices
+    float calculateDistance(int u, int v);
 
     // Função para imprimir a representação da lista de adjacência do grafo
     void printGraph();
@@ -36,8 +57,14 @@ public:
     // Função para imprimir as coordenadas dos vértices
     void printCoordinates();
 
-    // Função para calcular a distância euclidiana entre dois vértices
-    double calculateDistance(int u, int v);
+    // Função para imprimir os pares portais
+    void printPortal();
+
+    // Função para imprimir os pares de vértices
+    void printEdges();
+
+    // Função que implementa o algoritmo de Dijkstra e verifica se o destino é alcançável
+    bool canReachWithEnergyAndPortals(int start, int end, float energy, int maxPortals);
 
     // Funções de acesso
     int getNumVertices();
