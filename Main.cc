e0d413b8db19749b85fe4b6a2c42d0947fcd23f9
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2024-07-15 23:00:52 -0300

    Update Main.cc

--- Main.cc
+++ Main.cc
@@ -1,17 +1,34 @@
+/******************************************************************************
+
+5 4 2
+0 1
+0 0
+0 2
+1 1
+10 2
+0 1
+0 3
+1 4
+2 4
+0 2
+3 4
+1 1
+
+
+*******************************************************************************/
 #include "Functions.h"
 #include <iostream>
 
 int main()
 {
-    int n; // grafos ou vertices
-    int m; // arestas ou trilhas
-    int k; // portais
-    int energia; // energia do personagem 
-    int quantidade_de_portais; //quantidade de portais que o personagem pode usar 
+    int n;
+    int m;
+    int k;
+    int energia;
+    int quantidade_de_portais;
     std::cin >> n >> m >> k;
     Graph grafo(n);
 
-    // leitura das coordenadas
     for (int i = 0; i < n; i++)
     {
         float x, y;
@@ -19,7 +36,6 @@ int main()
         grafo.setCoordinate(i, x, y);
     }
 
-    // leitura de adjacencias
     for (int i = 0; i < m; i++)
     {
         int g1, g2;
@@ -27,23 +43,35 @@ int main()
         grafo.addEdge(g1, g2);
     }
 
-    // leitura de portais
-    for (int i = 0; i < k; i++){
-        int g1, g2; 
-        std::cin >> g1 >> g2; 
+    for (int i = 0; i < k; i++)
+    {
+        int g1, g2;
+        std::cin >> g1 >> g2;
         grafo.addPortal(g1, g2);
     }
-    
-    std::cin >> energia >> quantidade_de_portais;
-    
-    int start = 0; // vértice inicial
-    int end = n - 1; // vértice final
 
-    if (grafo.canReachWithEnergyAndPortals(start, end, energia, quantidade_de_portais)) {
-        std::cout << "1 1" << std::endl;
-    } else {
-        std::cout << "0 0" << std::endl;
-        }
+    std::cin >> energia >> quantidade_de_portais;
+
+    int start = 0;
+    int end = n - 1;
+
+    if (grafo.algoritmo_dijkstra_lista(start, end, energia, quantidade_de_portais))
+    {
+        std::cout << "1 ";
+    }
+    else
+    {
+        std::cout << "0 ";
+    }
+
+    if (grafo.algoritmo_aestrela_lista(start, end, energia, quantidade_de_portais))
+    {
+        std::cout << "1" << std::endl;
+    }
+    else
+    {
+        std::cout << "0" << std::endl;
+    }
 
     return 0;
 }
